b34c2cf80e07c04bdfe620a95b0a731431396a72
Miquéias <miqueiaswebdesign@gmail.com>
2020-07-29 13:14:09 -0300

    alterações

--- src/pages/Home/App.js
+++ src/pages/Home/App.js
@@ -1,19 +1,20 @@
 import React from 'react';
-import Menu from './components/Menu'
-import dadosIniciais from './data/dados_iniciais.json'
-import BannerMain from './components/BannerMain'
-import Carousel from './components/Carousel'
-import Footer from './components/Footer'
+import Menu from '../../components/Menu'
+import dadosIniciais from '../../data/dados_iniciais.json'
+import BannerMain from '../../components/BannerMain'
+import Carousel from '../../components/Carousel'
+import Footer from '../../components/Footer'
 
 
 
-function App() {
+
+function Home() {
   return (
     <div>
       <Menu />
 
       <BannerMain 
-        videoTitle={dadosIniciais.categorias[0].videos[0].titulo[0]} 
+        videoTitle={dadosIniciais.categorias[0].videos[0].titulo} 
         url={dadosIniciais.categorias[0].videos[0].url} 
         videoDescription={"O que é Front-end? Trabalhando na área os termos HTML, CSS e JavaScript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa!"}
       />
@@ -43,9 +44,9 @@ function App() {
         category={dadosIniciais.categorias[5]}
       />  
 
-
+      <Footer />
      </div>
   );
 }
 
-export default App;
+export default Home;
